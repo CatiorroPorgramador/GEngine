f7043a90a83cd5567d7e6f62511b8ddcd9423c6a
Gustavo Cavalcanti <gustavolisndo@gmail.com>
2022-03-20 20:01:29 -0300

    Main

--- src/Engine/Sprite.java
+++ src/Engine/Sprite.java
@@ -5,17 +5,22 @@ import java.awt.Rectangle;
 import java.awt.Color;
 import java.awt.Image;
 
+import java.awt.image.BufferedImage;
+
 import javax.swing.ImageIcon;
 
 public class Sprite extends Entity
 {
     // Appearance
     public Image texture = null;
-
+    public int frame, hframes, vframes;    // ..., Horizontal Frames, Vertical Frames
+    
     // Constructor
     public Sprite(Rectangle rect)
     {
         this.rect = rect;
+        hframes = 1;
+        vframes = 1;
     }
 
     // Methods
@@ -24,16 +29,22 @@ public class Sprite extends Entity
         texture = new ImageIcon(texture_filename).getImage();
     }
     
-    public void load_spritesheet(String texture_filename) {}
+    public void load_spritesheet(String texture_filename, int hframes, int vframes) 
+    {
+        texture = new ImageIcon(texture_filename).getImage();
+        this.hframes = hframes;
+        this.vframes = vframes;
+
+        BufferedImage a;
+
+        
+    }
 
     @Override
     public void render(Graphics2D graphics)
     {
         if (texture != null)
-        {
             graphics.drawImage(this.texture, rect.x, rect.y, rect.width, rect.height, null);
-        }
-        
         else
         {
             graphics.setColor(Color.BLUE);
